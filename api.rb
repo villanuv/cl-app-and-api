5cca70825da37050ff967375e09e5df6a380f576
Patrick Villanueva <villanuv@yahoo.com>
2019-03-20 03:18:18 -0500

    GET /records/name works

--- api.rb
+++ api.rb
@@ -43,3 +43,21 @@ get '/records/birthdate' do
 		}
 	end.to_json
 end
+
+get '/records/name' do
+	interface = Interface.new
+	interface.load_data("people.csv")
+	interface.load_data("people_pipe.csv")
+	interface.load_data("people_space.csv")
+
+	all_records = interface.sort_by_last_name_desc
+	all_records.map do |person| 
+		{
+			first_name: person.first_name,
+			last_name: person.last_name,
+			gender: person.gender,
+			color: person.color,
+			birthdate: person.birthdate.strftime("%m/%d/%Y")
+		}
+	end.to_json
+end
