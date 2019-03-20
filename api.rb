d013dced5f0ba456c7d59f09ecde846318f3049a
Patrick Villanueva <villanuv@yahoo.com>
2019-03-20 03:36:36 -0500

    refactored api

--- api.rb
+++ api.rb
@@ -4,17 +4,19 @@ require './person'
 require './interface'
 
 
+before do
+	@interface = Interface.new
+	@interface.load_data("people.csv")
+	@interface.load_data("people_pipe.csv")
+	@interface.load_data("people_space.csv")
+end
+
 get '/' do
 	'Guaranteed Rate API'
 end
 
 get '/records/gender' do
-	interface = Interface.new
-	interface.load_data("people.csv")
-	interface.load_data("people_pipe.csv")
-	interface.load_data("people_space.csv")
-
-	all_records = interface.sort_by_gender_and_last_name
+	all_records = @interface.sort_by_gender_and_last_name
 	all_records.map do |person| 
 		{
 			first_name: person.first_name,
@@ -27,12 +29,7 @@ get '/records/gender' do
 end
 
 get '/records/birthdate' do
-	interface = Interface.new
-	interface.load_data("people.csv")
-	interface.load_data("people_pipe.csv")
-	interface.load_data("people_space.csv")
-
-	all_records = interface.sort_by_birthdate
+	all_records = @interface.sort_by_birthdate
 	all_records.map do |person| 
 		{
 			first_name: person.first_name,
@@ -45,12 +42,7 @@ get '/records/birthdate' do
 end
 
 get '/records/name' do
-	interface = Interface.new
-	interface.load_data("people.csv")
-	interface.load_data("people_pipe.csv")
-	interface.load_data("people_space.csv")
-
-	all_records = interface.sort_by_last_name_desc
+	all_records = @interface.sort_by_last_name_desc
 	all_records.map do |person| 
 		{
 			first_name: person.first_name,
