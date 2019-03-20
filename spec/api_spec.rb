ab57fb3734e9661fa2200acd973357f97e94cc97
Patrick Villanueva <villanuv@yahoo.com>
2019-03-20 13:30:21 -0500

    refactored api tests

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -12,31 +12,60 @@ describe 'Guaranteed Rate API' do
 		Sinatra::Application
 	end
 
-	it "has a default homepage" do
-		get '/'
-		expect(last_response).to be_ok
-		expect(last_response.body).to eq 'Guaranteed Rate API'
+	describe "homepage" do
+		it "starter route included" do
+			get '/'
+			expect(last_response).to be_ok
+			expect(last_response.body).to eq 'Guaranteed Rate API'
+		end
 	end
 
-	it "GET /records/gender" do
-		get '/records/gender'
-		expect(last_response).to be_ok
-		body = JSON.parse(last_response.body)
-		all_keys = body.map{ |person| person.keys }[0]
-		expect(all_keys).to match_array(["first_name", "last_name", "gender", "color", "birthdate"])
+	describe "GET /records/gender" do
+		before do
+			get '/records/gender'
+		end
+
+		it "returns response" do
+			expect(last_response).to be_ok
+		end
+
+		it "returns json" do
+			body = JSON.parse(last_response.body)
+			all_keys = body.map{ |person| person.keys }[0]
+			expect(all_keys).to match_array(["first_name", "last_name", "gender", "color", "birthdate"])
+		end
 	end
 
-	it "GET /records/birthdate" do
-		get '/records/birthdate'
-		expect(last_response).to be_ok
-		all_keys = body.map{ |person| person.keys }[0]
-		expect(all_keys).to match_array(["first_name", "last_name", "gender", "color", "birthdate"])
+	describe "GET /records/birthdate" do
+		before do
+			get '/records/birthdate'
+		end
+
+		it "returns response" do
+			expect(last_response).to be_ok
+		end
+
+		it "returns json" do
+			body = JSON.parse(last_response.body)
+			all_keys = body.map{ |person| person.keys }[0]
+			expect(all_keys).to match_array(["first_name", "last_name", "gender", "color", "birthdate"])
+		end
 	end
 
-	it "GET /records/name" do
-		get '/records/name'
-		expect(last_response).to be_ok
-		all_keys = body.map{ |person| person.keys }[0]
-		expect(all_keys).to match_array(["first_name", "last_name", "gender", "color", "birthdate"])
+
+	describe "GET /records/name" do
+		before do
+			get '/records/name'
+		end
+
+		it "returns response" do
+			expect(last_response).to be_ok
+		end
+
+		it "returns json" do
+			body = JSON.parse(last_response.body)
+			all_keys = body.map{ |person| person.keys }[0]
+			expect(all_keys).to match_array(["first_name", "last_name", "gender", "color", "birthdate"])
+		end
 	end
 end
